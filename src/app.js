c8afd3f4f2c4b0feacb316e627721e1d8632ac04
XK <kai.xie@tarsocial.com>
2020-06-18 11:10:22 +0800

    add redux and eslint

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './app.less';
-import svg from './assets/img/报表.svg';
+// import svg from './assets/img/报表.svg';
 
 
 function App() {
   return (
     <div className="App">
+      1
     </div>
   );
 }
